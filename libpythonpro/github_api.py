72e0b52f5f6da2d21cc3fb19256669a392d71772
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-13 03:46:18 -0300

    Corrigido flake8

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -5,7 +5,7 @@ def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no GitHub
     :param usuario: str com o nome de usuário no github
-    :return:str com o link do avatar
+    :return:str com o link do avatar.
     """
     url = f' https://api.github.com/users/{usuario}'
     resp = requests.get(url)
